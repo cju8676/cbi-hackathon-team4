49642c6f61a32579382915fe3f10bfec7039ccdd
Corey Urbanke <cju8676@rit.edu>
2023-07-13 10:33:30 -0400

    brand buttons using MUI

--- src/App.js
+++ src/App.js
@@ -4,31 +4,40 @@ import logo from './logo.svg';
 import cbi from './cbi.png';
 import './App.css';
 import test from './image';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 
 function App() {
+  const [brand, setBrand] = useState('corona');
   const [pic, setPic] = useState('');
 
-  function askGPT(topic) {
+  function askGPT(topic, brand) {
+    // mock generated ChatGPT responses
     const prompts = [
         {
+            "brand" : "corona",
             "topic" : "summer",
             "description" : "A graphic of a Corona bottle and lime slice against a beach backdrop, with surfboards and palm trees that morph into a cityscape.",
             "tagline" : "Corona Extra - Where the beach meets the city."
         },
         {
-            "topic" : "summer",
-            "description" : "A frosty Corona bottle is placed on a background which is half a heat map of a bustling city and half a cool-toned beach scene",
-            "tagline" : "Bring the cool wherever you go with Corona"
-        },
-        {
+            "brand" : "modelo",
             "topic" : "sports",
             "description" : "diverse group of people, each engaging in different activities (e.g., playing soccer, dancing, cooking), all holding a Modelo. The overall design forms the shape of a Modelo bottle",
             "tagline" : "Different Stories, United by Modelo",
         },
+        {
+            "brand" : "robertmondavi",
+            "topic" : "wedding",
+            "description" : "",
+            "tagline" : ""
+        }
     ]
     // GPT call goes here
-    return prompts;
+    return prompts.find((prompt) => prompt.topic === topic && prompt.brand === brand);
   }
 
   function testGetImage() {
@@ -67,9 +76,23 @@ function App() {
       <img style={{width: '50%'}} src={cbi} alt="CBi Logo"></img>
       <h1>CBI Hackathon</h1>
       <h2>Social Media Marketing Generator</h2>
+
+      <h4>Select a CBI Brand you want to market</h4>
+      <ToggleButtonGroup
+        value={brand}
+        onChange={(event, newBrand) => {
+          setBrand(newBrand);
+        }}
+        aria-label="brand"
+        exclusive
+      >
+        <ToggleButton value="corona" aria-label="corona">Corona</ToggleButton>
+        <ToggleButton value="modelo" aria-label="modelo">Modelo</ToggleButton>
+        <ToggleButton value="robertmondavi" aria-label="robertmondavi">Robert Mondavi</ToggleButton>
+      </ToggleButtonGroup>
       <p>Enter a topic/activity you want to market:</p>
-      <input />
-      <button onClick={() => testGetImage()}>Generate Image</button>
+      <TextField variant='outlined' label="Topic"/>
+      <Button onClick={() => testGetImage()}>Generate Image</Button>
       {pic && <img src={"data:image/png;base64," + pic} alt="Generated Image"></img>}
       {!pic && <p>Click the button to generate an image!</p>}
     </div>
